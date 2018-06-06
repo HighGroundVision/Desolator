34027eae74024b88e8744e4a60ba58b12f9e83ea
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2018-06-06 19:32:53 -0400

    turning on stuff in production

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   API_BASE_URL: '"https://hgv-tarrasque.azurewebsites.net/api/"',
   FLAG_CONSTRUCTION: true,
-  FLAG_AUTHENTICATION: false,
-  FLAG_SOCIAL: false,
-  FLAG_KARMA: false
+  FLAG_AUTHENTICATION: true,
+  FLAG_SOCIAL: true,
+  FLAG_KARMA: true
 }
