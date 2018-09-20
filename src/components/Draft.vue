443f31eabd673dcf74eae53d171d11097e6f4ed1
Jamie Webster <jamie@densyfy.com>
2018-09-20 14:39:34 -0400

    Updated the draft page with details as to why it was disabled.

--- src/components/Draft.vue
+++ src/components/Draft.vue
@@ -3,6 +3,14 @@
     <b-row>
       <b-col>
         <h2>Drafting</h2>
+        <p>
+          We have currently disabled drafting based on feedback from our users.
+          It takes too long currently to enter all the details needed to complete the process.
+          To the point most people could not enter the all the details needed before the draft starts.
+        </p>
+        <p>
+          We are still exploring other options and may bring this back in the future.
+        </p>
       </b-col>
     </b-row>
   </section>
