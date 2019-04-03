b9c34ba5cbf2b886e317b0107815aad292e9321f
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2019-04-02 22:06:59 -0400

    moved web.config to public added static to sub assets config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 var RemoveServiceWorkerPlugin = require('webpack-remove-serviceworker-plugin')
 
 module.exports = {
+  assetsSubDirectory: 'static',
   configureWebpack: {
     plugins: [
       new RemoveServiceWorkerPlugin({ filename: 'service-worker.js' })
